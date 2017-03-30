5970698d5bc0575203f5d518bf4888a648c0d0c9
Siobhán K Cronin <siobhankcronin@gmail.com>
2017-03-30 13:19:28 -0700

    Delete central_limit_theorem.py

--- Prep/HackerRank/central_limit_theorem.py
+++
@@ -1,9 +0,0 @@
-# Central Limit Theorem
-
-import math
-
-def less_than_boundary_cdf(x, mean, std):
-    return round(0.5 * (1 + math.erf((x - mean)/ (std * math.sqrt(2)))), 4)
-
-print less_than_boundary_cdf(9800, 49 * 205, math.sqrt(49) * 15)
-
