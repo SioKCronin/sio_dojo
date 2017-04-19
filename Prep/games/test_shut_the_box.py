34e75a99c058f1770673dcf7c3ec978c8a30e51d
SioKCronin <siobhankcronin@gmail.com>
2017-04-18 18:00:28 -0700

    Completed methods for the game

--- Prep/games/test_shut_the_box.py
+++ Prep/games/test_shut_the_box.py
@@ -1,13 +1,21 @@
 # Test Shut The Box
 
 import unittest
-import shut_the_box
+from shut_the_box import Box, combinations
 
 class TestBox(unittest.TestCase):
     def test_create_box(self):
         box = Box()
 
-print "test shut the box", __name__
+    def test_box_starting_value(self):
+        box = Box()
+        self.assertEqual(box.status, range(1,13))
+
+class TestCombinations(unittest.TestCase):
+    def test_combinations(self):
+        self.assertEqual(combinations(7, [1,2,3, 4,5,6,7,8]),\
+                set([(7,), (1, 6), (2, 5), (3, 4), (1, 2, 4)]))
+
 if __name__ == '__main__':
     unittest.main()
 
