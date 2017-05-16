7ede1e638915ca861644c56041f510f8ca5ca750
SioKCronin <siobhankcronin@gmail.com>
2017-05-16 13:38:14 -0700

    Array development

--- prep/data_structures/2d_arrays.py
+++ prep/data_structures/2d_arrays.py
@@ -1,3 +1,35 @@
 # 2D Arrays
 
+import numpy as np
+
+def sum_hourglasses(matrix):
+    sums = []
+    for i in range(0,4):
+        for j in range(0,4):
+            value = (matrix[i][j] +  
+                    matrix[i][j+1] + 
+                    matrix[i][j+2] + 
+                    matrix[i+1][j+1] + 
+                    matrix[i+2][j] + 
+                    matrix[i+2][j+1] + 
+                    matrix[i+2][j+2])
+            sums.append(value)
+
+    # There has to be three rows below 
+    # Top left index must be less or equal to len(row) - 3
+
+    return max(sums)
+
+# Import the data into a matrix
+count = 0
+data = []
+while count < 6:
+    data.append([int(i) for i in raw_input().strip().split(' ')])
+    count += 1
+
+# Call function with imported data
+
+print sum_hourglasses(data)
+
+
 
---
+++ prep/data_structures/dynamic_array.py
@@ -0,0 +1,29 @@
+# Dynamic Array
+
+lastAns = 0
+
+def query_reader(queries, n):
+    for i range(n):
+        if queries[i][0] = 1:
+
+
+
+
+def query_two
+
+# Import the data into a matrix
+n, q = [int(i) for i in raw_input().strip().split(' '))]
+row = 0
+queries = []
+while row < q:
+    queries.append([int(i) for i in raw_input().strip().split(' ')])
+    row += 1
+
+seqList = [[] for i in range(n)]
+
+# Call function with imported data
+
+print sum_hourglasses(data)
+
+
+
