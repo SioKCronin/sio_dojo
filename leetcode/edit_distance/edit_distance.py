3f12bc7f4e4cce45f9f34236980f5e2745e33969
SioKCronin <siobhankcronin@gmail.com>
2018-06-28 12:48:20 -0700

    Updates

--- leetcode/edit_distance/edit_distance.py
+++ leetcode/edit_distance/edit_distance.py
@@ -4,6 +4,8 @@ class TestEditDistance(unittest.TestCase):
 
     def test_simple(self):
         self.assertEqual(edit_distance("bee", "bear"), 2)
+        self.assertEqual(edit_distance("horse", "ros"), 3)
+        self.assertEqual(edit_distance("intention", "execution"), 5)
 
 def edit_distance(x, y):
 
@@ -13,19 +15,19 @@ def edit_distance(x, y):
     rem = 0
 
     def loop(x, y, add, mod, rem):
-        # Define the base case of the recurrence 
-        if len(x) > len(y):
-            return [add, mod, rem]
-        if len(x) < len(y):
-            return [add, mod, rem]
-        for i in range(n):
-            add += 1 + loop(x[i:], y[i+1:], add, mod, rem)[0]
-            mod += (x[0] != y[0]) + loop(x[i+1:], y[i+1:], add, mod, rem)[1]
-            rem += 1 + loop(x[i+1:], y[i:], add, mod, rem)[2]
-        print([add, mod, rem])
+        if (not x) or (not y):
+            return min(add + abs(len(x)-len(y)), mod, rem)
+        if len(x) == 1 and len(y) == 1:
+            if x == y:
+                return min(add, mod, rem)
+            return min(add, mod + 1, rem)
 
-    loop(x, y, add, mod, rem)
-    return min(add, mod, rem)
+        for i in range(n-1):
+            add += 1 + loop(x[i:], y[i+1:], add, mod, rem)
+            mod += (x[0] != y[0]) + loop(x[i+1:], y[i+1:], add, mod, rem)
+            rem += 1 + loop(x[i+1:], y[i:], add, mod, rem)
+
+    return loop(x, y, add, mod, rem)
 
 if __name__ == '__main__':
     unittest.main()
