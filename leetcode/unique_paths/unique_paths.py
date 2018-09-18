23e62d9bd3c1e04f7adb932060123827ee2c9d8c
SioKCronin <siobhankcronin@gmail.com>
2018-09-17 18:15:01 -0700

    WIP

--- leetcode/unique_paths/unique_paths.py
+++ leetcode/unique_paths/unique_paths.py
@@ -3,17 +3,13 @@ import unittest
 class TestUniquePaths(unittest.TestCase):
 
     def test_leet_example(self):
-        self.assertEqual(unique_paths(3, 2, 0, 0), 3)
-        self.assertEqual(unique_paths(7, 3, 0, 0), 28)
+        self.assertEqual(unique_paths(3, 2, 1, 1), 3)
+        self.assertEqual(unique_paths(7, 3, 1, 1), 28)
 
 def unique_paths(m, n, i, j):
 
-    if i > m or j > n:
-        return 0
-
-    if i == m and j == n:
-        return 1
-
+    if i > m or j > n: return 0
+    if i == m and j == n: return 1
     return unique_paths(m, n, i+1, j) + unique_paths(m, n, i, j+1)
 
 if __name__ == '__main__':
